4570ee5c4b754b4ac41d1167354f1c8dec591340
Heron Feijao <heronfeijao@gmail.com>
2022-05-30 19:04:11 -0300

    exercise 7 finished

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Darth", last_name: "Vader", hourly_rate: 60)
+@store1.employees.create(first_name: "Heron", last_name: "Feijao", hourly_rate: 60)
+@store2.employees.create(first_name: "Luke", last_name: "Skywalker", hourly_rate: 150)
+@store2.employees.create(first_name: "Anakin", last_name: "Skywalker", hourly_rate: 100)
+@store2.employees.create(first_name: "Yoda", last_name: "Master",  hourly_rate: 80)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,12 +1,20 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
-require_relative './exercise_6'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
+require_relative "./exercise_5"
+require_relative "./exercise_6"
 
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "What's your store name? >"
+@store_name = gets.chomp
+
+@store_create = Store.create(
+  name: @store_name,
+)
+
+puts @store_create.errors.full_messages
